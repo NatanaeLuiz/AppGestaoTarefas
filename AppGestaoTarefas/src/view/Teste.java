52a1cecd376b5234b1cb26be5ad7c75710b13736
NatanaeLuiz <natanael.10@hotmail.com>
2021-03-24 01:10:34 -0300

    #000001-MAS-natanaelrolim-commit-cria-conexao-banco-postgres
    - Foi criado a coneção com o banco postgress
    - Foi criado a estrutura do projeto (MVC)

---
+++ AppGestaoTarefas/src/view/Teste.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import controller.TarefaController;
+import model.Tarefa;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		try {
+			Tarefa t1 = new Tarefa();
+			t1.setTitulo("Incluir dados no banco");
+			t1.setDescricao("Deve incluir os dados no banco");
+			t1.setResponsavel("Natanael Luiz Rolim");
+			t1.setPrioridade("Baixa");
+			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); 
+			Date data;
+			data = formato.parse("24/03/2021");
+			t1.setData(data);
+			
+			//Instancia Controller
+			TarefaController con = new TarefaController();
+			
+			//GRAVA TAREFA
+			con.salva(t1);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
