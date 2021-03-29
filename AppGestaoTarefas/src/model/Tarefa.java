a0ab72e31f693941af0226b72bc4f5d5d88ee225
NatanaeLuiz <natanael.10@hotmail.com>
2021-03-29 01:59:37 -0300

    #000002-MAS-natanaelrolim-commit-projeto
    - Foi criado o metodo CRIAR
    - Foi criado o metodo LISTAR
    - Foi criado o metodo UPDATE
    - Foi criado o metodo DELETAR

--- AppGestaoTarefas/src/model/Tarefa.java
+++ AppGestaoTarefas/src/model/Tarefa.java
@@ -8,15 +8,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="TAREFA")
+//@ManagedBean(value = "tarefaBean")
 public class Tarefa {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (unique = true, nullable = false)
 	private int id;
 	
 	@Column
@@ -32,7 +32,6 @@ public class Tarefa {
 	@Column
 	private String prioridade;
 	
-	@Temporal(TemporalType.DATE)
 	@Column
 	private Date data;
 
--- AppGestaoTarefas/src/view/Teste.java
+++ AppGestaoTarefas/src/view/Teste.java
@@ -11,25 +11,25 @@ import model.Tarefa;
 public class Teste {
 	public static void main(String[] args) {
 		
-		try {
-			Tarefa t1 = new Tarefa();
-			t1.setTitulo("Incluir dados no banco");
-			t1.setDescricao("Deve incluir os dados no banco");
-			t1.setResponsavel("Natanael Luiz Rolim");
-			t1.setPrioridade("Baixa");
-			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); 
-			Date data;
-			data = formato.parse("24/03/2021");
-			t1.setData(data);
-			
-			//Instancia Controller
-			TarefaController con = new TarefaController();
-			
-			//GRAVA TAREFA
-			con.salva(t1);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Tarefa t1 = new Tarefa();
+//			t1.setTitulo("Incluir dados no banco");
+//			t1.setDescricao("Deve incluir os dados no banco");
+//			t1.setResponsavel("Natanael Luiz Rolim");
+//			t1.setPrioridade("Baixa");
+//			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); 
+//			Date data;
+//			data = formato.parse("24/03/2021");
+//			t1.setData(data);
+//			
+//			//Instancia Controller
+//			TarefaController con = new TarefaController();
+//			
+//			//GRAVA TAREFA
+////			con.salva(t1);
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
